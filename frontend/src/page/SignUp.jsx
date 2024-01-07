0c2e6dc0025b715b2d6649c6c2ed3a9daec2a6fc
khanalisha73017 <152811469+khanalisha73017@users.noreply.github.com>
2024-01-08 01:22:36 +0530

    Update SignUp.jsx

--- frontend/src/page/SignUp.jsx
+++ frontend/src/page/SignUp.jsx
@@ -7,11 +7,13 @@ import { createUser } from "../redux/authuser/action";
 
 export const SignUp = () => {
   const [formData, setFormData] = useState({
+  
     name: "",
+
     email: "",
     password: "",
   });
-  const [avatar, setAvatar] = useState(null);
+  const [avatar, setavatar] = useState(null);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -20,7 +22,7 @@ export const SignUp = () => {
     if (type === "file") {
       const fileInput = e.target;
       if (fileInput.files) {
-        setAvatar(fileInput.files[0]);
+        setavatar(fileInput.files[0]);
       }
     } else {
       setFormData({
@@ -52,7 +54,7 @@ export const SignUp = () => {
               Create and account
             </h1>
             <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
-              <div>
+{/*               <div>
                 <label
                   htmlFor="avatar"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -67,7 +69,7 @@ export const SignUp = () => {
                   onChange={handleChange}
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary_green focus:border-primary_green block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 />
-              </div>
+              </div> */}
               <div>
                 <label
                   htmlFor="username"
@@ -163,4 +165,3 @@ export const SignUp = () => {
     </section>
   );
 };
-
