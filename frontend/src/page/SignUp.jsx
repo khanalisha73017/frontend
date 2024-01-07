f4f82e0fca110538554ff62d25d2aa8680210d49
khanalisha73017 <152811469+khanalisha73017@users.noreply.github.com>
2024-01-08 01:11:30 +0530

    Update SignUp.jsx

--- frontend/src/page/SignUp.jsx
+++ frontend/src/page/SignUp.jsx
@@ -54,7 +54,7 @@ export const SignUp = () => {
               Create and account
             </h1>
             <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
-              <div>
+{/*               <div>
                 <label
                   htmlFor="avatar"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -69,7 +69,7 @@ export const SignUp = () => {
                   onChange={handleChange}
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary_green focus:border-primary_green block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 />
-              </div>
+              </div> */}
               <div>
                 <label
                   htmlFor="username"
